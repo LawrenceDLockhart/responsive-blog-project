34fb03c10896c4de6f3424297a5952c9d5c32913
Lawrence Lockhart <lawrence@vaadin.com>
2024-03-08 16:30:54 -0600

    Add formview with 4 buttons

---
+++ src/main/java/com/example/application/views/helloworld/FormView.java
@@ -0,0 +1,19 @@
+package com.example.application.views.helloworld;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.router.Route;
+
+@Route("form")
+public class FormView extends FormLayout {
+    public FormView() {
+        Button button1 = new Button("Up", VaadinIcon.ARROW_UP.create());
+        Button button2 = new Button("Down", VaadinIcon.ARROW_DOWN.create());
+        Button button3 = new Button("Left", VaadinIcon.ARROW_LEFT.create());
+        Button button4 = new Button("Right", VaadinIcon.ARROW_RIGHT.create());
+        FormLayout formLayout = new FormLayout(button1, button2, button3, button4);
+        add(formLayout);
+    }
+
+}
